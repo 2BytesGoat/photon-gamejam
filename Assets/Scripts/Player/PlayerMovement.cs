2bd5cdbaac22ce5c4f495eaf9777f0db7deccf44
Paul Vacariu <paul.vacariu@tradeshift.com>
2022-09-22 15:48:05 +0200

    Added lobby and rooms (#1)
    * Added lobby and rooms
    
    * Clean unwanted files

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -22,7 +22,7 @@ public class PlayerMovement : MonoBehaviour {
     }
 
     private void FixedUpdate() {
-        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        rb.MovePosition(rb.position + moveSpeed * Time.fixedDeltaTime * movement);
     }
 
     private void UpdateFacingDirection() {
