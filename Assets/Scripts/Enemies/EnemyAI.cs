2aaa0e586b2cbc6643a556d7cd062949336fda5c
GianiStatie <gianistatie@gmail.com>
2022-09-24 19:56:10 +0300

    Added automatic player detection on FOV entered

--- Assets/Scripts/Enemies/EnemyAI.cs
+++ Assets/Scripts/Enemies/EnemyAI.cs
@@ -1,13 +1,16 @@
+using System;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class EnemyAI : MonoBehaviour {
-    [SerializeField] public Transform target;
-    private NavMeshAgent agent;
+    public float detectionRadiusSize;
+    public LayerMask detectionLayer;
+
+    private NavMeshAgent agent; 
+    private Transform target;
 
     private void Start() {
-        // target = GameObject.FindGameObjectWithTag("Player").transform;
-
         agent = GetComponent<NavMeshAgent>();
         agent.updateRotation = false;
         agent.updateUpAxis = false;
@@ -15,12 +18,27 @@ public class EnemyAI : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
+        DetectObject();
+        
+        if (target == null) {
+            return;
+        }
+
         agent.SetDestination(target.position);
 
         if (agent.velocity.x < 0.01f) {
             this.transform.localScale = new Vector3(-1, 1, 1);
         } else if (agent.velocity.x > 0.01f) {
             this.transform.localScale = new Vector3(1, 1, 1);
+        }
+    }
+
+    private void DetectObject() {
+        Collider2D collider = Physics2D.OverlapCircle(this.transform.position, detectionRadiusSize, detectionLayer);
+        if (collider != null) {
+            target = collider.transform;
+        } else {
+            target = null;
         }
     }
 }
