8f7a4d526bad3007635b60b0ed60a03ceedf968f
Ratoone <vacariu.paul@gmail.com>
2022-09-26 21:03:50 +0200

    Max players MP

--- Assets/Scripts/Constants.cs
+++ Assets/Scripts/Constants.cs
@@ -3,12 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Constants {
-    public const float ASTEROIDS_MIN_SPAWN_TIME = 5.0f;
-    public const float ASTEROIDS_MAX_SPAWN_TIME = 10.0f;
-
-    public const float PLAYER_RESPAWN_TIME = 4.0f;
-
     public const int PLAYER_MAX_LIVES = 3;
+    public const int MAX_PLAYER_COUNT = 6;
 
     public const string PLAYER_LIVES = "PlayerLives";
     public const string PLAYER_READY = "IsPlayerReady";
